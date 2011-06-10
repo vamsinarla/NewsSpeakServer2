d7317f9a30af7df7ee12e4757d40f9a8dc82c099
Your Name <vamsi.narla@gmail.com>
2011-06-10 10:58:57 -0700

    Adding feed metadata to json response

--- FeedHandler.py
+++ FeedHandler.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.api import urlfetch
@@ -46,9 +30,14 @@ class FeedHandler(webapp.RequestHandler):
                                      entry.summary)
             item_json = feeditem.feeditemjsonencoder().default(item)
             feed_items.append(item_json)
-            
-        self.response.out.write(json.JSONEncoder().encode(feed_items))
 
+        feed_info = parsed_feed.feed
+        self.response.out.write(json.dumps({
+                                            "title": feed_info.title,
+                                            "language": feed_info.language,
+                                            "subtitle":feed_info.subtitle,
+                                            "entries": feed_items }))
+        
 def main():
     application = webapp.WSGIApplication([('/feed', FeedHandler)],
                                          debug=True)
